f5335226cde4c773885a9dcd938633aba42ac6af
josedancruz <josedanielcruz4@gmail.com>
2026-05-10 00:08:15 -0400

    added base leaflet map, made restapi middleman to show location of lightposts on map

---
+++ src/api.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from databases import Database
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from contextlib import asynccontextmanager
+import os
+
+load_dotenv()
+database_url = os.getenv("DATABASE_URL", "")
+database = Database(database_url)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.disconnect()
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/lightposts")
+async def get_lightposts():
+    query = """
+    SELECT light_post_id, latitude, longitude, city
+    FROM light_post
+    """
+
+    rows = await database.fetch_all(query)
+    return [dict(row) for row in rows]
