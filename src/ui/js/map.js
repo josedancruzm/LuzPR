28d17ff9904de9ef7cbc84eab7771e67327b99ca
josedancruzm <jose.cruz190@upr.edu>
2026-05-16 18:50:49 -0400

    added report modal, removed side bar, added same log-in header

--- src/ui/js/map.js
+++ src/ui/js/map.js
@@ -32,9 +32,9 @@ fetch("http://localhost:8000/lightposts")
                 <b>Coordinates:</b> ${row.longitude}, ${row.latitude}<br>
                 <b>City:</b> ${row.city}<br>
 
-                <div class="popup-footer" onclick="toggleReportWindow()">
+                <div class="popup-footer">
                     <span title="Report">    
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-flag-icon lucide-flag"><path d="M4 22V4a1 1 0 0 1 .4-.8A6 6 0 0 1 8 2c3 0 5 2 7.333 2q2 0 3.067-.8A1 1 0 0 1 20 4v10a1 1 0 0 1-.4.8A6 6 0 0 1 16 16c-3 0-5-2-8-2a6 6 0 0 0-4 1.528"/></svg>
+                        <svg onclick="showReportDialog()" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-flag-icon lucide-flag"><path d="M4 22V4a1 1 0 0 1 .4-.8A6 6 0 0 1 8 2c3 0 5 2 7.333 2q2 0 3.067-.8A1 1 0 0 1 20 4v10a1 1 0 0 1-.4.8A6 6 0 0 1 16 16c-3 0-5-2-8-2a6 6 0 0 0-4 1.528"/></svg>
                     </span>
                 </div>
             `);
@@ -43,3 +43,19 @@ fetch("http://localhost:8000/lightposts")
 
         map.addLayer(markers);
     });
+
+//function declaration to create modal
+const dialog = document.getElementById("report-dialog")
+
+//opens modal
+function showReportDialog() {
+    dialog.showModal()
+}
+
+//closes modal
+function closeReportDialog() {
+    dialog.close()
+}
+
+//when you click outside of the modal, close the modal
+dialog.addEventListener("click", () => dialog.close())
